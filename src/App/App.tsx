3da4eaedc01bef43b40437be9d95fe7e90faf235
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-10 23:49:42 -0400

    Created nav bar.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+
+// Components
 import Layout from "./components/Layout/Layout";
+import Navigation from "./components/Navigation/Navigation";
 
 const App = () => <Layout>
-    <div style={{backgroundColor: "green"}}>Test 1</div>
+    <Navigation/>
     <div style={{backgroundColor: "blue"}}>Test 2</div>
     <div style={{backgroundColor: "cyan"}}>Test 3</div>
 </Layout>;
