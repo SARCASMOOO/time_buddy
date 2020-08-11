42f33240809d408f0800ba7b24139b8694835a63
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-11 00:15:44 -0400

    Added side drawer to nav bar.

--- src/App/components/Navigation/Navigation.tsx
+++ src/App/components/Navigation/Navigation.tsx
@@ -11,6 +11,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import SideMenu from "./SideMenu/SideMenu";
 
 // Interfaces
 interface Props {
@@ -39,7 +40,7 @@ const Navigation = ({}: Props) => {
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon/>
+                        <SideMenu> <MenuIcon/> </SideMenu>
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         Logo
---
+++ src/App/components/Navigation/SideMenu/SideMenu.tsx
@@ -0,0 +1,78 @@
+import React, {ReactNode} from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+type Anchor = 'left';
+
+interface Props {
+    children: ReactNode;
+}
+
+const SideMenu = ({children} : Props) => {
+    const [state, setState] = React.useState({
+        left: false,
+    });
+
+    const toggleDrawer = (anchor: Anchor, open: boolean) => (
+        event: React.KeyboardEvent | React.MouseEvent,
+    ) => {
+        if (
+            event.type === 'keydown' &&
+            ((event as React.KeyboardEvent).key === 'Tab' ||
+                (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor: Anchor) => (
+        <div
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}>
+            <List>
+                {['Time Buddy'].map((text, index) => (
+                    <ListItem style={{width: '300px'}} button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider/>
+            <List>
+                {['Sign In', 'Sign Up'].map((text, index) => (
+                    <ListItem style={{width: '300px'}} button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            {(['left'] as Anchor[]).map((anchor) => (
+                <React.Fragment key={anchor}>
+                    <Button style={{color: "white"}} onClick={toggleDrawer(anchor, true)}>{children}</Button>
+                    <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                        {list(anchor)}
+                    </Drawer>
+                </React.Fragment>
+            ))}
+        </div>
+    );
+}
+
+export default SideMenu;
