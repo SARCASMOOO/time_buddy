cb43674793cdff31b0859f99b20a7921abaeede8
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-11 17:34:47 -0400

    Removed search component to change to firebase.

--- src/App/components/Navigation/SelectSchool/Search/Search.tsx
+++ src/App/components/Navigation/SelectSchool/Search/Search.tsx
@@ -1,35 +1,4 @@
 import React from "react";
 
-// @ts-ignore
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-// @ts-ignore
-import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui";
-// @ts-ignore
-import { Layout } from "@elastic/react-search-ui-views";
-
-import "@elastic/react-search-ui-views/lib/styles/styles.css";
-
-const connector = new AppSearchAPIConnector({
-    searchKey: "iRIaFPzyTIm8taNlsZr1AXgj",
-    engineName: "search-ui-examples",
-    hostIdentifier: "elastic"
-});
-
-const Search = () => {
-    return (
-        <SearchProvider
-            config={{
-                apiConnector: connector
-            }}
-        >
-            <div className="App">
-                <Layout
-                    header={<SearchBox />}
-                    bodyContent={<Results style={{overflow: "scrollX"}}titleField="title" urlField="nps_link" />}
-                />
-            </div>
-        </SearchProvider>
-    );
-}
-
+const Search = () => <h1>Test</h1>;
 export default Search;
