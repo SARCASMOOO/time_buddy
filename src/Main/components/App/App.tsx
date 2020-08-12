b9462215c4e50a5fae10c311ab3cac405825056f
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-11 22:44:28 -0400

    Added routing to app component. Added links to side drawer.

--- src/Main/components/App/App.tsx
+++ src/Main/components/App/App.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import './App.css';
 
 // Components
-import {BrowserRouter as Router} from 'react-router-dom';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import LandingPage from '../Landing/Landing';
+import SignUpPage from '../SignUp/SignUp';
+import SignInPage from '../SignIn/SignIn';
+import PasswordForgetPage from '../PasswordForget/PasswordForget';
+import HomePage from '../Home/Home';
+import AccountPage from '../Account/Account';
+import AdminPage from '../Admin/Admin';
 import Layout from "../Layout/Layout";
-import Navigation from "../NavBar/Navigation";
+import Navigation from "../Navigation/Navigation";
+
+// Constants
+import * as ROUTES from '../../constants/routes';
 
 const App = () => <Layout>
     <Router>
         <Navigation/>
-        <div style={{backgroundColor: "blue"}}>Test 2</div>
-        <div style={{backgroundColor: "cyan"}}>Test 3</div>
+        <main>
+            <Switch>
+                <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+                <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+                <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
+                <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+                <Route path={ROUTES.HOME} component={HomePage}/>
+                <Route path={ROUTES.ACCOUNT} component={AccountPage}/>
+                <Route path={ROUTES.ADMIN} component={AdminPage}/>
+            </Switch>
+        </main>
+        <footer>This is the footer</footer>
     </Router>
 </Layout>;
 
--- src/Main/components/Navigation/Navigation.tsx
+++ src/Main/components/Navigation/Navigation.tsx
@@ -1,5 +1,55 @@
 import React from "react";
 
-const Navigation = () => <h1>Navigation</h1>;
+// Styles
+import classes from './Navigation.module.css';
+
+// Components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import SideMenu from "./SideMenu/SideMenu";
+import SelectSchool from "./SelectSchool/SelectSchool";
+
+// Interfaces
+interface Props {}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+    }),
+);
+
+const Navigation = ({}: Props) => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar style={{height: '100%', backgroundColor: '#02baa8'}} position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <SideMenu> <MenuIcon /> </ SideMenu >
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Logo
+                    </Typography>
+                    <SelectSchool/>
+                </Toolbar>
+            </AppBar>
+        </div>);
+}
 
 export default Navigation;
+
+
