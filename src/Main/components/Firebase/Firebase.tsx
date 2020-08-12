953de5c8a4df4217ed5341add98cfeffe9bf35aa
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-11 23:03:12 -0400

    Configured web app with firebase project.

--- src/Main/components/Firebase/Firebase.tsx
+++ src/Main/components/Firebase/Firebase.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import app from 'firebase/app';
 
-const Firebase = () => (
-    <div>
-        <h1>App</h1>
-    </div>
-);
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ5YJomxPVeXOjWU3eW01tQNulMG3gPpw",
+    authDomain: "time-buddy-f7b1a.firebaseapp.com",
+    databaseURL: "https://time-buddy-f7b1a.firebaseio.com",
+    projectId: "time-buddy-f7b1a",
+    storageBucket: "time-buddy-f7b1a.appspot.com",
+    messagingSenderId: "2885386463",
+    appId: "1:2885386463:web:3397cb5c6e0be8e4df696a"
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+    }
+}
 
 export default Firebase;
