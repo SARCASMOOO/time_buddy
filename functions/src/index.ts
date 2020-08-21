de96e21d1b0b1d27bd372e4678fa14bb1d47adbb
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-21 15:56:13 -0400

    Tested firebase function.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 // import * as functions from 'firebase-functions';
+// const axios = require('axios').default;
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const updateCourses = functions.https.onRequest((request, response) => {
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
 // });
 
 export default {};
