3233e4f56ab908001afcb31622874676d9765aa1
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-09-16 21:48:48 -0400

    Made navigation menu responsive.

--- src/Main/components/Navigation/Navigation.tsx
+++ src/Main/components/Navigation/Navigation.tsx
@@ -1,53 +1,15 @@
 import React from "react";
 
 // Styles
-import classes from './Navigation.module.css';
+import classes from './Navigation.module.scss';
 
-// Components
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-import SideMenu from "./SideMenu/SideMenu";
-import SelectSchool from "./SelectSchool/SelectSchool";
-
-// Interfaces
-interface Props {}
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            flexGrow: 1,
-        },
-    }),
-);
-
-const Navigation = ({}: Props) => {
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <AppBar style={{height: '100%', backgroundColor: '#02baa8'}} position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <SideMenu> <MenuIcon /> </ SideMenu >
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Time Buddy
-                    </Typography>
-                    <SelectSchool/>
-                </Toolbar>
-            </AppBar>
-        </div>);
-}
+const Navigation = (props: {}) => (
+    <div className={classes.navigation}>
+        <div className={classes.navigation__logo}>
+            <h2>Time Buddy</h2>
+        </div>
+        <div className={classes.navigation__dropdown}><h2>DropDown</h2></div>
+    </div>);
 
 export default Navigation;
 
