a20dc3b8027808c00ea8f48c846224cc43e39f76
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-09-17 01:52:33 -0400

    Created desktop version of nav component.

--- src/Main/components/Navigation/Navigation.tsx
+++ src/Main/components/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {pure} from 'recompose';
 
 // Styles
 import classes from './Navigation.module.scss';
@@ -6,16 +7,23 @@ import classes from './Navigation.module.scss';
 // Components
 import Dropdown from "./Dropdown/Dropdown";
 
+const NavList = () => {
+    const Items = ['Login', 'Sign Up', 'Time Table22'].map((navItem: string) => <h1 key={navItem}>{navItem}</h1>);
+    return (<div>{Items}</div>);
+}
+
 const Navigation = (props: {}) => (
     <div className={classes.navigation}>
         <div className={classes.navigation__logo}>
             <h2>Time Buddy</h2>
         </div>
         <div className={classes.navigation__dropdown}>
-            <Dropdown />
+            {/* FIXME: This doesn't work if you start on desktop then resize down. */}
+            {window.innerWidth < 460 ? <Dropdown/> : <NavList/>}
         </div>
     </div>);
 
-export default Navigation;
-
+export default pure(Navigation);
+// TODO: Make a react hook to monitor screen width to choose
+//  which component to render based on screen size.
 
