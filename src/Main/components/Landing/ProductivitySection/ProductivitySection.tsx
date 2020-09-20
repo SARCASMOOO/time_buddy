a763fed1fade193e0d4bbc66cf265884e67cc6dc
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-09-19 20:23:10 -0400

    Changed image styling.

--- src/Main/components/Landing/ProductivitySection/ProductivitySection.tsx
+++ src/Main/components/Landing/ProductivitySection/ProductivitySection.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import image from '../../../assets/images/land-sm.jpg';
 
 import classes from './ProductivitySection.module.scss';
 
 function ProductivitySection(props: {}) {
     return (
         <section className={classes.productivity__back_ground}>
+            <img src={image} className={classes.productivity__img} alt="Background image of productivity."/>
         </section>
     );
 }
